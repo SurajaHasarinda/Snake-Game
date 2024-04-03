b9912fce2e52a2b24aed6482f5cc3b9b4e6544d7
Suraja Hasarinda <surajahasarinda@gmail.com>
2024-04-04 00:01:08 +0530

    Snake game

---
+++ src/Game/GameFrame.java
@@ -0,0 +1,15 @@
+package Game;
+
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame {
+    GameFrame(){
+        GamePanel panel = new GamePanel();
+        this.add(panel);
+        this.setTitle("Snake Game");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+}
---
+++ src/Game/SnakeGame.java
@@ -0,0 +1,7 @@
+package Game;
+
+public class SnakeGame {
+    public static void main(String[] args) {
+       GameFrame frame = new GameFrame();
+    }
+}
